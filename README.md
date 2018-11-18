6bccd6d430ace1a9479c6d74fb4361adda9a6468
Marcus Johansson <majohansson@gmail.com>
2018-11-18 07:22:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+hyper-gcp-statusline
+====================
+
+A status line plugin for Hyper.js that shows the currently acxtive Google Cloud Platform project and Kubernetes context.
+
+![hyper-gcp-statusline](https://cloud.githubusercontent.com/assets/xyz "hyper-gcp-statusline")
+
+## Install
+
+Add the following to your `~/.hyper.js` config:
+
+```javascript
+module.exports = {
+  ...
+  plugins: ['hyper-gcp-statusline']
+  ...
+}
+```
+
+## Config
+
+Add the following to `~/.hyper.js`
+
+### Default location to `gcloud`and `kubectl`
+Unless specified below, `hyper-gcp-statusline`assumes the tools are installed in the global `PATH`.
+
+```javascript
+module.exports = {
+  config: {
+    ...
+      hyperGcpStatusLine: {
+        gcloudBinary: '/my/path/gcloud',
+        kubectlBinary: '/my/path/gcloud'
+
+      }
+    ...
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,115 @@
+const { execFile } = require('child_process');
+
+/*
+todo:
+- få upp på github 
+- fixa css och ikoner
+- twittra
+*/
+
+let configuration = {
+    gcloudBinary: 'gcloud',
+    kubectlBinary: 'kubectl'
+};
+
+let status = {
+    gcp: 'n/a',
+    kubernetes: 'n/a'
+}
+
+function setStatusGcp() {
+    runCommand(configuration.gcloudBinary, ['config', 'get-value', 'project']).then(project => {
+        status.gcp = project;
+    }).catch(() => {
+        status.gcp = 'n/a';
+    })
+}
+
+function setStatusKubernetes() {
+    runCommand(configuration.kubectlBinary, ['config', 'current-context']).then(context => {
+        runCommand(configuration.kubectlBinary, ['config', 'view', '--minify', '--output', 'jsonpath={..namespace}']).then(namespace => {
+            status.kubernetes = context + ":" + namespace;
+        }).catch(() => {
+            status.kubernetes = context + ":default";
+        })
+    }).catch(() => {
+        status.kubernetes = 'n/a';
+    })
+}
+
+function runCommand(command, options) {
+    return new Promise((resolve, reject) => {
+        execFile(command, options, (error, stdout, stderr) => {
+            if (error) {
+                reject(`${error}\n${stderr}`)
+            }
+            if (stdout.trim() == "") {
+                reject("stdout was empty");
+            }
+            resolve(stdout.trim())
+        })
+    })
+}
+
+exports.reduceUI = (state, { type, config }) => {
+    switch (type) {
+        case 'CONFIG_LOAD':
+        case 'CONFIG_RELOAD': {
+            configuration = Object.assign(configuration, config.hyperGcpStatusLine);
+        }
+    }
+
+    return state
+}
+
+exports.decorateHyper = (Hyper, { React }) => {
+    return class extends React.PureComponent {
+        constructor(props) {
+            super(props);
+            this.state = {};
+        }
+
+        render() {
+            const { customChildren } = this.props
+            const existingChildren = customChildren ? customChildren instanceof Array ? customChildren : [customChildren] : [];
+
+            return (
+                React.createElement(Hyper, Object.assign({}, this.props, {
+                    customInnerChildren: existingChildren.concat(React.createElement('div', { className: 'hyper-gcp-status-line' },
+                        React.createElement('div', { className: 'item gcp' }, this.state.gcp),
+                        React.createElement('div', { className: 'item kubernetes' }, this.state.kubernetes)
+                    ))
+                }))
+            );
+        }
+
+        componentDidMount() {
+            this.interval = setInterval(() => {
+                this.setState(status);
+            }, 100);
+        }
+
+        componentWillUnmount() {
+            clearInterval(this.interval);
+        }
+    };
+}
+
+exports.middleware = (store) => (next) => (action) => {
+    switch (action.type) {
+        case 'SESSION_ADD_DATA':
+            const { data } = action;
+            if (data.indexOf('\n') > 1) {
+                setStatusGcp();
+                setStatusKubernetes();
+            }
+            break;
+
+        case 'SESSION_SET_ACTIVE':
+            setStatusGcp();
+            setStatusKubernetes();
+            break;
+    }
+
+    next(action);
+}
