52b3a48a75e412a5c2c84cd698a8b316e6ab58a9
Marcus Johansson <majohansson@gmail.com>
2018-11-19 21:47:26 +0100

    fixed cut icon

--- index.js
+++ index.js
@@ -105,7 +105,6 @@ exports.decorateConfig = (config) => {
             }
             .hyper-gcp-status-line .item {
                 padding: 2px 15px 0 25px;
-                display: inline-table;
             }
             .hyper-gcp-status-line .item:last-child {
                 margin-left: auto;
